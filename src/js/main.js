8cae5bbb4069b0be283fb1e8f0b621e3c0966833
laindomJS <adrijar21@gmail.com>
2022-10-29 15:58:01 -0400

    feat: added recognition

--- src/js/main.js
+++ src/js/main.js
@@ -1,9 +1,32 @@
-window.addEventListener('load', () => {
-  if (!webkitSpeechRecognition || webkitSpeechRecognition === undefined) {
-    alert('Your browser not supports voice recognition!');
-  }
+const COLORS = ['red', 'black', 'white', 'blue'];
+const button = document.querySelector('#btn');
+
+if (!webkitSpeechRecognition || webkitSpeechRecognition === undefined) {
+  alert('Your browser not supports voice recognition!');
+}
+
+function defineRecognition() {
   const recognition = new webkitSpeechRecognition();
-  recognition.lang = 'es-ES';
-  recognition.continuous = true;
-  console.log(recognition);
+  const body = document.querySelector('body');
+
+  recognition.lang = 'en-US';
+  recognition.continuous = false;
+
+  recognition.onresult = evt => {
+    let color = evt.results[0][0].transcript;
+    body.style.backgroundColor = color;
+    console.log(`Color is ${color}`);
+  }
+
+  recognition.start();
+
+  recognition.onspeechend = () => recognition.stop();
+  recognition.onnomatch = () => alert('No color recognised');
+  recognition.onerror = (evt) => {
+    console.log(`Error occurred in recognition ${evt.error}`);
+  }
+}
+
+button.addEventListener('click', () => {
+  defineRecognition();
 })
